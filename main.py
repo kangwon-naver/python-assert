f614d93fec4cb21e7027fc4e3ba6300b0f08ed2d
KangWon LEE <kangwon.lee.edu@gmail.com>
2021-05-28 06:54:10 +0000

    added separator lines

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ def practice_assert(arg):
   assert arg, f"arg == {arg!r}"
 
 
-print("this will pass")
+print("this will pass ".ljust(40, '='))
 practice_assert("abc")
 print('''I said "This will pass"''')
 
 
 try:
-  print("This will raise an exception")
+  print("This will raise an exception ".ljust(40, '='))
   practice_assert('')
 except AssertionError as e:
   print('''I told you "This will raise an exception" :''', e)
@@ -17,6 +17,6 @@ finally:
   print("end of try-except block")
 
 
-print("this will raise an exception")
+print("this will raise an exception ".ljust(40, '='))
 practice_assert([])
 print('''This line will not show up''')
