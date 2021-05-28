def51b5cc21b6b4a0eaf01ec29bfcd4b91b091f8
KangWon LEE <kangwon.lee.edu@gmail.com>
2021-05-28 06:31:10 +0000

    calling the function multiple times

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 def practice_assert(arg):
+  print(f"is {arg!r} True?", bool(arg))
   assert arg, f"arg == {arg!r}"
 
 
 print("this will pass")
 practice_assert("abc")
+print('''I said "This will pass"''')
+
+
+try:
+  print("This will raise an exception")
+  practice_assert('')
+except AssertionError as e:
+  print('''I told you "This will raise an exception" :''', e)
+finally:
+  print("end of try-except block")
+
 
 print("this will raise an exception")
 practice_assert([])
+print('''This line will not show up''')
